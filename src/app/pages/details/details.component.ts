4e74bcc3db22db053d8f10eb5deb48cf4a90b76b
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-21 17:55:26 -0300

    feat: Implementando lógica da página PokemonDetails

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
+import { PokeApiService } from 'src/app/service/poke-api.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
 
+  private pokemonUrl: string = 'https://pokeapi.co/api/v2/pokemon';
+  private nameUrl: string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon() {
+    const id = this.activeRoute.snapshot.params['id']
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.pokemonUrl}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.nameUrl}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
